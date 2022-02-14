d13e17ecbbec8b5c1ff8a1e4dfac107b89e5e8f4
TYama <alohaty81@gmail.com>
2022-02-14 10:29:30 -0700

    addToZero modified

--- addToZero.js
+++ addToZero.js
@@ -1,5 +1,5 @@
 // Starting array
-let array = [28, 43, -12, 30, 4, 0, 12]
+let array = [0, 21, 33, 6, -9, 0];
 
 // Write your solution below:
 let addToZero = false;
@@ -9,22 +9,27 @@ for(let i = 0; i < array.length; i++){
     let n = array[i];
     if(n === 0){
         //if it is 0, check if there is another 0
-        if(array.indexOf(0) !== -1){
-            addToZero = true;
-            console.log('There are two 0s in the array!');
-            break;
+        if(i !== array.length - 1){
+            if(array.indexOf(0, i+1) !== -1){
+                addToZero = true;
+                console.log(`There are two 0s in the array!`);
+                break;
+            }
         }
     }else{
         //if it is not 0, make a negative number of that number
         //check if the number exist
         if(array.indexOf(-n) !== -1){
-            addToZero = true;
+            addToZero = true; 
             console.log(`There are ${n} and ${-n} in the array!`);
             break;
         }
     }
 }
 
+if(!addToZero){
+    console.log("False...");
+}
 
 
 
